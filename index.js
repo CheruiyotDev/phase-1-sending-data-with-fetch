206fe93944c189f899c6da70ebcaf482326f65c3
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-14 17:56:40 +0300

    sending dat with fetch

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+fetch(destinationURL, configurationObject);
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+  function submitData(userName, userEmail) {
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail,
+      }),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        // Append the new id to the DOM
+        document.body.innerHTML += object.id;
+      })
+      .catch(function (error) {
+        // Handle errors
+        document.body.innerHTML += error.message;
+      });
+  }
+  
